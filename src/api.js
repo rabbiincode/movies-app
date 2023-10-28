20d220db21fee5f443c9b409d12c5f3b2b5855b6
RABBI <rabbiincode@gmail.com>
2023-10-28 02:40:05 +0100

    refactor

--- src/api.js
+++ src/api.js
@@ -4,8 +4,8 @@ const request = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   params: {
     //api_key: 'process.env.REACT_APP_API_KEY',
-    api_key: 'abb710499b8746c04d8c83045535443b',
-  },
+    api_key: 'abb710499b8746c04d8c83045535443b'
+  }
 })
 
 export default request;
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -43,7 +43,7 @@ const Card = ({movies, darkMode}) => {
             {
               userid: userId?.id,
               moviesid: [],
-              darkmode: false
+              darkmode: true
             }
           ])
         }
